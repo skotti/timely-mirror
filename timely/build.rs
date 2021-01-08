f0732f5782e6236a3e596dd02f8e04e8d5296826
Anastasiia Ruzhanskaia <anastasiia.ruzhanskaia@inf.ethz.ch>
2021-01-08 10:45:16 +0300

    build config for cpu

--- timely/build.rs
+++ timely/build.rs
@@ -1,4 +1,4 @@
 fn main() {
-    println!(r"cargo:rustc-link-search=/home/aruzhans/timely-on-fpga/timely");
+    println!(r"cargo:rustc-link-search=/home/skotti/timely_code_clion/timely-dataflow/timely");
     println!(r"cargo:rustc-link-lib=fpgalibrary");
 }
