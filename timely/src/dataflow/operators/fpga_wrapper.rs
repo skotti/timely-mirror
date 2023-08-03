79f5e64eda1a7393de3e3da830c04853b298876e
Georg Teufelberger <tgeorg@student.ethz.ch>
2023-08-03 15:13:19 +0200

    style: Prefix unused variables with `_`
    Part of cleaning up compiler warnings

--- timely/src/dataflow/operators/fpga_wrapper.rs
+++ timely/src/dataflow/operators/fpga_wrapper.rs
@@ -633,7 +633,7 @@ impl<S: Scope<Timestamp = u64>> FpgaWrapper<S> for Stream<S, u64> {
                 let mut current_length = 0;
                 let max_length = param * 8 + frontier_param * 8;
                 let data_length = param * 8;
-                let data_start_index = 0;
+                let _data_start_index = 0;
                 let progress_start_index = param_output * 8;
 
                 unsafe {
@@ -717,11 +717,11 @@ impl<S: Scope<Timestamp = u64>> FpgaWrapper<S> for Stream<S, u64> {
             }
 
             if !has_data {
-                let frontier_length = frontier_param * 8; //2 + ghost_indexes.len() + 4 * ghost_indexes.len();
+                let _frontier_length = frontier_param * 8; //2 + ghost_indexes.len() + 4 * ghost_indexes.len();
                 let mut current_length = 0;
                 let max_length = param * 8 + frontier_param * 8;
                 let data_length = param * 8;
-                let data_start_index = 0;
+                let _data_start_index = 0;
                 let progress_start_index = param_output * 8;
 
                 unsafe {
