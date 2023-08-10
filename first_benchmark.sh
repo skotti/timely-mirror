2fe2460b1cee9c578cfecc0882653f406e3e7428
Georg Teufelberger <tgeorg@student.ethz.ch>
2023-08-10 16:14:43 +0000

    bench: New benchmark Includes: - updated example code used - updated script to run - results

--- first_benchmark.sh
+++ first_benchmark.sh
@@ -4,7 +4,7 @@
 N=1000
 
 # Create the 'benchmarks' folder if it doesn't exist
-mkdir --parents benchmarks
+mkdir --parents benchmarks-v2
 
 # Compile
 cargo build --release --example first_benchmark
@@ -12,7 +12,7 @@ cargo build --release --example first_benchmark
 # Loop $N times
 for ((i=1; i<=$N; i++))
 do
-    output_file="benchmarks/output_$i.txt"  # Create the output file name with index and folder
+    output_file="benchmarks-v2/output_$i.txt"  # Create the output file name with index and folder
     sudo target/release/examples/first_benchmark > $output_file  # Run the binary and redirect output to the file
     echo "Run $i completed. Output stored in $output_file"
 done
--- timely/examples/first_benchmark.rs
+++ timely/examples/first_benchmark.rs
@@ -25,8 +25,9 @@ fn main() {
         let mut epoch_start = Instant::now();
         let mut hist = hdrhist::HDRHist::new();
 
-        let num_rounds = 10_000;
-        for round in 0..num_rounds {
+        const NUM_ROUNDS: u64 = 10_000;
+        let mut epoch_latencies: [u64; NUM_ROUNDS as usize] = [0; NUM_ROUNDS as usize];
+        for round in 0..NUM_ROUNDS {
             let number_of_inputs = 16;
             for _j in 0..number_of_inputs {
                 input.send(round + 21); // max = 0
@@ -39,20 +40,19 @@ fn main() {
             let epoch_nanos = (epoch_end - epoch_start).as_nanos();
             epoch_start = epoch_end;
             hist.add_value(epoch_nanos as u64);
+            epoch_latencies[round as usize] = epoch_nanos as u64;
         }
 
         let epoch_end = Instant::now();
         let total_nanos = (epoch_end - start).as_nanos();
-        let epoch_latency = (total_nanos as f64) / 1_000_000_000f64 / (num_rounds as f64); // sec
-        let epoch_throughput = (num_rounds as f64) / (total_nanos as f64) * 1_000_000_000f64; // epochs/sec
+        let epoch_latency = (total_nanos as f64) / 1_000_000_000f64 / (NUM_ROUNDS as f64); // sec
+        let epoch_throughput = (NUM_ROUNDS as f64) / (total_nanos as f64) * 1_000_000_000f64; // epochs/sec
         println!("epoch time: {}", epoch_latency);
         println!("total time (nanos): {}", total_nanos);
         println!("epoch throughput: {}", epoch_throughput);
-        println!("epoch latency:");
-        println!("percentile, lower_bound, upper_bound");
-        for elem in hist.summary() {
-            let (percentile, lower_bound, upper_bound) = elem;
-            println!("{percentile}, {lower_bound}, {upper_bound}")
+        println!("epoch latencies:");
+        for elem in epoch_latencies {
+            println!("{elem}");
         }
     })
     .unwrap();
