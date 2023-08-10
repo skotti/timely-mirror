16fce450e930b0307f5f0559d11deae8ae07b5e6
Georg Teufelberger <tgeorg@student.ethz.ch>
2023-08-10 22:56:41 +0200

    don't show index in csv

--- benchmarks/get-numbers.py
+++ benchmarks/get-numbers.py
@@ -25,16 +25,15 @@ output_files = [f for f in os.listdir(output_dir) if f.startswith('output_')]
 output_data = []
 
 for file_name in output_files:
-    index = int(re.search(r'output_(\d+)\.txt', file_name).group(1))
     file_path = os.path.join(output_dir, file_name)
     epoch_throughput, epoch_latency_0_5 = extract_info_from_file(file_path)
-    output_data.append((index, epoch_throughput, epoch_latency_0_5))
+    output_data.append((epoch_throughput, epoch_latency_0_5))
 
 csv_file = 'output_data.csv'
 
 with open(csv_file, 'w', newline='') as f:
     writer = csv.writer(f)
-    writer.writerow(['index', 'epoch_throughput', 'epoch_latency_0_5'])
+    writer.writerow(['epoch_throughput', 'epoch_latency_0_5'])
     writer.writerows(output_data)
 
 print(f"Data extracted and saved to '{csv_file}'")
