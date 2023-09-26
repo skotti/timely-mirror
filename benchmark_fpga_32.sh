c1023a1c819b636fe18aab1aa9b91ad956d3a802
Georg Teufelberger <tgeorg@student.ethz.ch>
2023-09-26 12:42:47 +0000

    feat: Source files for batch size 32 FPGA latency benchmark

---
+++ benchmark_fpga_32.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# loop count
+N=100
+
+# Create the 'benchmarks' folder if it doesn't exist
+mkdir --parents benchmarks/fpga-latency/batch_32/
+
+# Compile
+cargo build --release --example benchmark_fpga_32
+
+# Loop $N times
+for ((i=1; i<=$N; i++))
+do
+    output_file="benchmarks/fpga-latency/batch_32/output_$i.txt"  # Create the output file name with index and folder
+    sudo target/release/examples/benchmark_fpga_32 > $output_file  # Run the binary and redirect output to the file
+    echo "Run $i completed. Output stored in $output_file"
+done
+
+echo "All runs completed."
